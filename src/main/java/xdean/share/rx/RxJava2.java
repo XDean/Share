df1947be3bb2e73af8af49f61cba65c4e6a1edc2
XDean <373216024@qq.com>
2018-05-23 15:33:08 +0800

    add sample

--- src/main/java/xdean/share/rx/RxJava2.java
+++ src/main/java/xdean/share/rx/RxJava2.java
@@ -1,6 +1,12 @@
 package xdean.share.rx;
 
+import static xdean.jex.util.lang.ExceptionUtil.uncheck;
+
 import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.rxjavafx.schedulers.JavaFxScheduler;
+import io.reactivex.schedulers.Schedulers;
+import javafx.application.Platform;
 
 public class RxJava2 {
   public static void main(String[] args) {
@@ -9,5 +15,24 @@ public class RxJava2 {
         .map(i -> i * i)
         .reduce((a, b) -> a + b)
         .subscribe(e -> System.out.println(e));
+  }
+  
+  public static void schedule() {
+    new Thread(() -> {
+      int value = calc();
+      Platform.runLater(() -> {
+        // UI work
+        System.out.println(value);
+      });
+    }).start();
+    Observable.fromCallable(() -> calc())
+        .subscribeOn(Schedulers.computation())
+        .observeOn(JavaFxScheduler.platform())
+        .subscribe(e -> System.out.println(e));
+  }
+
+  public static int calc() {
+    uncheck(() -> Thread.sleep(1000));
+    return 1;
   }
 }
