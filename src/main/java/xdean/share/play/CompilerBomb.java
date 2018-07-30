52d0923423cfdebc9587aad89bdcdc6bf91c2fea
XDean <373216024@qq.com>
2018-07-30 16:54:39 +0800

    add compiler bomb

---
+++ src/main/java/xdean/share/play/CompilerBomb.java
@@ -0,0 +1,13 @@
+package xdean.share.play;
+
+// comment out these code to let your eclipse BOMB!
+
+// interface Type<T> {}
+//
+// class C implements Type<Type<? super C>> {}
+// class D<P> implements Type<Type<? super D<D<P>>>> {}
+//
+// class Test {
+// Type<? super C> c = new C();
+// Type<? super D<Byte>> d = new D<Byte>();
+// }
