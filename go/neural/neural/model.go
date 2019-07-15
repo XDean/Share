886d88bc9e74edab6c8b8fed4373462ac7ee52dd
dxu <dean.xu@asml.com>
2019-07-15 08:41:52 +0800

    model bp

--- go/neural/neural/model.go
+++ go/neural/neural/model.go
@@ -5,14 +5,15 @@ import "errors"
 type (
 	Model struct {
 		Config struct {
-			LayerCount int
-			NodeCount  []int
-			Activation Activation
+			LayerCount   int
+			NodeCount    []int
+			Activation   Activation
+			LearningRate float64
 		}
 		Value struct {
 			Error  float64
 			Node   [][]Node
-			Weight [][][]Weight
+			Weight [][][]Weight // first index starts from 1
 			Delta  [][]float64
 		}
 	}
@@ -28,6 +29,10 @@ type (
 		Nabla float64
 	}
 )
+
+func (m Model) Init() {
+
+}
 
 func (m Model) Feed(input []float64) error {
 	if len(input) != m.Config.NodeCount[0] {
@@ -47,15 +52,47 @@ func (m Model) Forward() {
 				sum += kn.Output * m.Value.Weight[l][k][i].Value
 			}
 			in.Input = sum
-			in.Output, in.Partial = m.Config.Activation.Active(in.Input)
+			if l == m.Config.LayerCount-1 {
+				in.Output, in.Partial = in.Input, 1
+			} else {
+				in.Output, in.Partial = m.Config.Activation.Active(in.Input)
+			}
 		}
 	}
 }
 
-func (m Model) CalcError() {
-
+func (m Model) CalcError(target []float64) {
+	m.Value.Error = 0
+	for i, v := range m.Value.Node[m.Config.LayerCount-1] {
+		m.Value.Error += (v.Output - target[i]) * (v.Input - target[i]) / 2
+	}
 }
 
-func (m Model) CalcGradient() {
+func (m Model) Backward(target []float64) {
+	for l := m.Config.LayerCount - 1; l > 0; l-- {
+		for j := 0; j < m.Config.NodeCount[l]; j++ {
+			if l == m.Config.LayerCount-1 {
+				m.Value.Delta[l][j] = (m.Value.Node[l][j].Output - target[j]) * m.Value.Node[l][j].Partial
+			} else {
+				sum := 0.0
+				for k := 0; k < m.Config.NodeCount[l+1]; k++ {
+					sum += m.Value.Delta[l+1][k] * m.Value.Weight[l+1][j][k].Value
+				}
+				m.Value.Delta[l][j] = sum * m.Value.Node[l][j].Partial
+			}
+			for i := 0; i < m.Config.NodeCount[l-1]; i++ {
+				m.Value.Weight[l][i][j].Nabla = m.Value.Delta[l][j] * m.Value.Node[l-1][i].Output
+			}
+		}
+	}
+}
 
+func (m Model) Learn() {
+	for l := 1; l < m.Config.LayerCount; l++ {
+		for i := 0; i < m.Config.NodeCount[l-1]; i++ {
+			for j := 0; j < m.Config.NodeCount[l]; j++ {
+				m.Value.Weight[l][i][j].Value -= m.Config.LearningRate * m.Value.Weight[l][i][j].Nabla
+			}
+		}
+	}
 }
