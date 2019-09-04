45c38ba56ca99a4afaba77c3a33e5b334954d494
dxu <dean.xu@asml.com>
2019-09-04 17:03:37 +0800

    autowired add map

--- src/spring-boot/2/inject/src/main/java/xdean/share/spring/inject/autowired/BeanConsumer.java
+++ src/spring-boot/2/inject/src/main/java/xdean/share/spring/inject/autowired/BeanConsumer.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import xdean.share.spring.inject.autowired.Beans.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Component
@@ -22,10 +23,16 @@ public class BeanConsumer {
     }
 
     @Autowired
-    private void inject(Optional<BeanD> d, List<BeanE> es) {
+    private void inject(Optional<BeanD> d, List<BeanE> list, Map<String, BeanE> map) {
         System.out.println("Inject with d: " + d);
-        System.out.println("Inject with es: " + es);
+        System.out.println("Inject with list: " + list);
+        System.out.println("Inject with map: " + map);
         System.out.println("Now b:" + b);
         System.out.println("Now c:" + c);
+    }
+
+    @Autowired
+    public void injectFail(BeanD d){
+        System.out.println("never happen");
     }
 }
