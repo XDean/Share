24da5905d597f473677089fbb0f8a544d0cc3ecb
Dean Xu <373216024@qq.com>
2020-04-28 20:49:23 +0800

    spring boot async

---
+++ src/spring-boot/async/src/main/java/xdean/share/spring/async/Application.java
@@ -0,0 +1,71 @@
+package xdean.share.spring.async;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@SpringBootApplication
+@EnableAsync
+public class Application {
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+        TaskService service = ctx.getBean(TaskService.class);
+
+        service.runVoid(0);
+        service.runVoid(1);
+
+        service.runFuture(2);
+        service.runFuture(3);
+
+        service.runCompletableFuture(4);
+        service.runCompletableFuture(5);
+
+        service.runListenableFuture(6);
+        service.runListenableFuture(7);
+
+        service.runQualifier(8);
+        service.runQualifier(9);
+
+        service.runError(10);
+        service.runError(11);
+
+        Thread.sleep(1000);
+        System.exit(0);
+    }
+
+    @Bean
+    public TaskExecutor executor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("MyPool");
+        executor.initialize();
+        return executor;
+    }
+
+    @Bean
+    @Qualifier("special")
+    public TaskExecutor specialExecutor() {
+        return new ConcurrentTaskExecutor();
+    }
+
+    @Bean
+    public AsyncConfigurer exceptionHandler() {
+        return new AsyncConfigurer() {
+            @Override
+            public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+                return (ex, method, params) -> System.out.printf("Error: %s %s\n", method, ex);
+            }
+        };
+    }
+}
---
+++ src/spring-boot/async/src/main/java/xdean/share/spring/async/TaskService.java
@@ -0,0 +1,47 @@
+package xdean.share.spring.async;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+@Component
+public class TaskService {
+
+    @Async
+    public void runVoid(int i) {
+        System.out.printf("%s run-%d\n", Thread.currentThread(), i);
+    }
+
+    @Async
+    public Future<String> runFuture(int i) {
+        System.out.printf("%s runFuture-%d\n", Thread.currentThread(), i);
+        return new AsyncResult<>("runFuture-" + i);
+    }
+
+    @Async
+    public CompletableFuture<String> runCompletableFuture(int i) {
+        System.out.printf("%s runCompletableFuture-%d\n", Thread.currentThread(), i);
+        return CompletableFuture.completedFuture("runCompletableFuture-" + i);
+    }
+
+    @Async
+    public ListenableFuture<String> runListenableFuture(int i) {
+        System.out.printf("%s runListenableFuture-%d\n", Thread.currentThread(), i);
+        return new AsyncResult<>("runListenableFuture-" + i);
+    }
+
+    @Async("special")
+    public void runQualifier(int i) {
+        System.out.printf("%s runListenableFuture-%d\n", Thread.currentThread(), i);
+    }
+
+    @Async
+    public void runError(int i) {
+        System.out.printf("%s runError-%d\n", Thread.currentThread(), i);
+        throw new IllegalArgumentException();
+    }
+}
